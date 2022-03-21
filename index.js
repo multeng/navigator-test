06b2732a3a8cfd1d9738030d26f95abc27ef1b73
Vladimir Kormakov <cenizo.kht@gmail.com>
2022-03-21 11:42:19 +0300

    feat: add vendor

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const nav = document.querySelector('.navigator');
 const userAgent = document.querySelector('.user-agent');
 const appCodeName = document.querySelector('.app-code-name');
+const vendor = document.querySelector('.vendor')
 
 
 
 window.onload = () => {
   nav.innerText = navigator;
   userAgent.innerText = navigator.userAgent;
+  vendor.innerText = navigator.vendor;
 }
