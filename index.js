2375fd13018f88f0442cd4816feb0436a18749ee
Vladimir Kormakov <cenizo.kht@gmail.com>
2022-03-28 10:37:45 +0300

    feature: add detect lib

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const nav = document.querySelector('.navigator');
 const userAgent = document.querySelector('.user-agent');
 const appCodeName = document.querySelector('.app-code-name');
-const vendor = document.querySelector('.vendor')
+const vendor = document.querySelector('.vendor');
+const mobile = document.querySelector('.mobile');
 
 
 
@@ -9,4 +10,7 @@ window.onload = () => {
   nav.innerText = navigator;
   userAgent.innerText = navigator.userAgent;
   vendor.innerText = navigator.vendor;
+  appCodeName.innerText = navigator.appCodeName;
+  const user = detect.parse(navigator.userAgent);
+  mobile.innerText = `Famaly version: ${user.browser.family}; Browser version: ${user.browser.version}; OS name: ${user.os.name}`
 }
