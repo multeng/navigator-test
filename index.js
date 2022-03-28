97b2bf699f3aec5226dda985fc898b5d30f9b27a
Vladimir Kormakov <cenizo.kht@gmail.com>
2022-03-28 15:28:17 +0300

    feat: add element

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const nav = document.querySelector('.navigator');
-const userAgent = document.querySelector('.user-agent');
+const userAgentElement = document.querySelector('.user-agent');
 const appCodeName = document.querySelector('.app-code-name');
 const vendor = document.querySelector('.vendor');
 const mobile = document.querySelector('.mobile');
+const isChrome = document.querySelector('.isChrome');
 
 
 
 window.onload = () => {
+  const userAgent = navigator.userAgent.toLowerCase;
   nav.innerText = navigator;
-  userAgent.innerText = navigator.userAgent;
+  userAgentElement.innerText = userAgent;
   vendor.innerText = navigator.vendor;
   appCodeName.innerText = navigator.appCodeName;
   const user = detect.parse(navigator.userAgent);
+  isChrome.innerText = /chrome/.test(userAgent);
   mobile.innerText = `Famaly version: ${user.browser.family}; Browser version: ${user.browser.version}; OS name: ${user.os.name}`
 }
